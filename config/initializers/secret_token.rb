55b0cdb8623fee422170b9523b3c4736038903f5
Liz Wiltsie <lizwiltsie@gmail.com>
2014-01-19 20:41:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moneyduo::Application.config.secret_key_base = '909e4cb0740d8daba78e4dbb42ce91b3dce34ce23b1ac0500190bfbae3874c406fac700f6810fed9981f6a46a84fd7e800bce2668f8f8eaa697e824aaf9f00c5'
